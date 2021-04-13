8e0a784c7171a88739414494b4c5218ba7643999
Antoine <antoineg.work@gmail.com>
2021-04-13 02:23:25 +0200

    Mise au propre pour multiple

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Page d\'accueil, tout marche bien ! Incroyable 2');
+    res.send('<h1>L\'application est en ligne et fonctionne');
 });
 
 app.get('/products', (req, res) => {
