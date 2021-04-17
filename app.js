e7377ff0fd53138bd2a7677311869a0823508568
Antoine <antoineg.work@gmail.com>
2021-04-17 22:52:14 +0200

    Version pour démonstration

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+
+
 const port = 3000;
 
+
 app.get('/', (req, res) => {
-    res.send('<h1>L\'application est en ligne et fonctionne, test 2');
+    res.sendFile(path.join(__dirname+'/index.html'));
+    
+});
+
+app.get('/products', (req, res) => {
+    res.send([
+        {
+            productId: '0',
+            productName: 'Pommes',
+            price: 2
+        },
+        {
+            productId: '1',
+            productName: 'Couscous',
+            price: 3
+        },
+        {
+            productId: '2',
+            productName: 'Caramel',
+            price: 4
+        },
+        {
+            productId: '3',
+            productName: 'Kebab',
+            price: 5
+        },
+    ])
 });
 
 app.get('/products', (req, res) => {
@@ -19,6 +50,58 @@ app.get('/products', (req, res) => {
     ])
 });
 
+app.get('/products', (req, res) => {
+    res.send([
+        {
+            productId: '0',
+            productName: 'Pommes',
+            price: 2
+        },
+        {
+            productId: '1',
+            productName: 'Couscous',
+            price: 3
+        },
+        {
+            productId: '2',
+            productName: 'Caramel',
+            price: 4
+        },
+        {
+            productId: '3',
+            productName: 'Kebab',
+            price: 5
+        },
+    ])
+});
+
+app.get('/clients', (req, res) => {
+    res.send([
+        {
+            clientId: '0',
+            clientName: 'Marsh',
+            clientFirstName: 'Stan'
+        },
+        {
+            clientId: '1',
+            clientName: 'McCormick',
+            clientFirstName: 'Kenny'
+        },
+        {
+            clientId: '2',
+            clientName: 'Kyle',
+            clientFirstName: 'Broflowski'
+        },
+        {
+            clientId: '0',
+            clientName: 'Stotch',
+            clientFirstName: 'Butters'
+        },
+    ])
+});
+
 app.listen(port, () => {
-    console.log(`Demo app is up and listening to port ${port}`);
+    console.log(`Prêt à écouter sur localhost:${port}`);
 })
+
+
